2f3a26fbf395b2398f0a7f98fb90ce231f69a293
James Forster <jamesalexforster@gmail.com>
2020-07-07 15:41:08 +0100

    second user story satisfied

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,10 +6,16 @@ describe('Airport', function() {
   });
 
   describe('Airport can accept a plane', function() {
-  
     it('Will dock a plane', function() {
       airport.dock(plane);
       expect(airport.hangar).toContain(plane);
+    });
+  });
+
+  describe('Airport can tell a plane to takeoff', function () {
+    it('Will get a plane to leave the hangar array', function () {
+      airport.dock(plane);
+      expect(airport.takeOff(plane)).toEqual("Plane has taken off succesfully")
     });
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -6,4 +6,15 @@ class Airport {
   dock(plane) {
     this.hangar.push(plane);
   }
+
+  takeOff(plane) {
+    var hangarLength = hangar.length();
+    this.hangar.pop();
+    if (this.hangar.length() < hangarLength) {
+      return "Plane has taken off succesfully";
+    }
+    else {
+      return "Take-off unsuccesful";
+    }
+  }
 }
